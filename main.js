791ba19934965e99ebdf7d885aa1db93531f346f
vutuananh2006 <vutuananh11102006@gmail.com>
2023-07-02 01:53:36 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var fruits = [
+    ["BANANA",15]
+    ["CUCUMBER",4]
+    ["WATERMELON",20]
+    ["JACKFRUIT",1]
+];
+console.log(fruits.join(', '))
+
+
+var myDog = {
+    "name": "Ngáo",
+
+    "legs": 4,
+
+    "freinds":["everything!"]
+
+};
+console.log(myDog.push.('color'))
